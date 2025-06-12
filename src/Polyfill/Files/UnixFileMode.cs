4d81b28dec7dab3f89448c650637c72c2459bde8
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-12 15:03:25 +0100

    create enum

---
+++ src/Polyfill/Files/UnixFileMode.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Polyfills;
+
+#if !NET7_0_OR_GREATER
+
+#if PolyPublic
+public
+#endif
+enum UnixFileMode
+{
+    None = 0,
+    OtherExecute = 1,
+    OtherWrite = 2,
+    OtherRead = 4,
+    GroupExecute = 8,
+    GroupWrite = 16,
+    GroupRead = 32,
+    UserExecute = 64,
+    UserWrite = 128,
+    UserRead = 256,
+    StickyBit = 512,
+    SetGroup = 1024,
+    SetUser = 2048
+}
+
+#endif
